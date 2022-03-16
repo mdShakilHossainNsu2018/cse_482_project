eb9e02716b028a36bfd61f49422deb4ff34d497f
mdShakilHossainNsu2018 <shakilnsu2018@gmail.com>
2022-03-16 15:23:51 +0600

    db test implimented

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require(getenv("ROOT")."global_constants.php");
 
 include "views/components/header/header.php";
 
+// home page
 echo "<h1> Hello world changed </h1>";
 
 include "views/components/footer/footer.php";
-
 ?>
