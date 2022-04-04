132720a21a349c2f07e9d30a6daec9dfb89b4eb5
mdShakilHossainNsu2018 <shakilnsu2018@gmail.com>
2022-04-04 11:42:14 +0600

    bootstrap added

--- views/components/header/header.php
+++ views/components/header/header.php
@@ -2,7 +2,6 @@
 require_once getenv("ROOT")."database/UserHelper.php";
 ?>
 
-
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
