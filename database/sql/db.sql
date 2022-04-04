3aa67330984d8b902555aadf67b1cee869559894
Basharat-Ziad <ziad.basharat59655@gmail.com>
2022-04-04 11:11:07 +0600

    sha

--- database/sql/db.sql
+++ database/sql/db.sql
@@ -18,11 +18,11 @@ USE `db` ;
 -- Table `db`.`user_profile`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db`.`user_profile` (
-  `profileID` INT NOT NULL,
+  `idprofile` INT NOT NULL,
   `number` VARCHAR(45) NOT NULL,
   `address` VARCHAR(45) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`profileID`))
+  PRIMARY KEY (`idprofile`))
 ENGINE = InnoDB;
 
 
@@ -30,16 +30,16 @@ ENGINE = InnoDB;
 -- Table `db`.`users`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db`.`users` (
-  `id` INT NOT NULL,
+  `iduser` INT NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `password` VARCHAR(256) NOT NULL,
   `CreatedAt` DATETIME NOT NULL,
-  `user_profile_profileID` INT NOT NULL,
-  PRIMARY KEY (`id`, `user_profile_profileID`),
-  INDEX `fk_users_user_profile1_idx` (`user_profile_profileID` ASC) VISIBLE,
-  CONSTRAINT `fk_users_user_profile1`
-    FOREIGN KEY (`user_profile_profileID`)
-    REFERENCES `db`.`user_profile` (`profileID`)
+  `user_profile_idprofile` INT NOT NULL,
+  PRIMARY KEY (`iduser`, `user_profile_idprofile`),
+  INDEX `fk_users_user_profile_idx` (`user_profile_idprofile` ASC) VISIBLE,
+  CONSTRAINT `fk_users_user_profile`
+    FOREIGN KEY (`user_profile_idprofile`)
+    REFERENCES `db`.`user_profile` (`idprofile`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -63,44 +63,26 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db`.`coords`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`coords` (
-  `idcoords` INT NOT NULL,
-  `lat` DOUBLE NOT NULL,
-  `lang` DOUBLE NOT NULL,
-  PRIMARY KEY (`idcoords`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `db`.`property_info`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db`.`property_info` (
   `idProperty` INT NOT NULL,
   `Location` VARCHAR(45) NOT NULL,
   `price` INT ZEROFILL NOT NULL,
-  `User_idUser` INT NOT NULL,
-  `User_table1_name` INT NOT NULL,
-  `Details_idDetails` INT NOT NULL,
-  `coords_idcoords` INT NOT NULL,
-  PRIMARY KEY (`idProperty`, `User_idUser`, `User_table1_name`, `Details_idDetails`),
-  INDEX `fk_property info_User1_idx` (`User_idUser` ASC, `User_table1_name` ASC) VISIBLE,
-  INDEX `fk_property info_Details1_idx` (`Details_idDetails` ASC) VISIBLE,
-  INDEX `fk_property_info_coords1_idx` (`coords_idcoords` ASC) VISIBLE,
-  CONSTRAINT `fk_property info_User1`
-    FOREIGN KEY (`User_idUser`)
-    REFERENCES `db`.`users` (`id`)
+  `users_iduser` INT NOT NULL,
+  `users_user_profile_idprofile` INT NOT NULL,
+  `details_idDetails` INT NOT NULL,
+  PRIMARY KEY (`idProperty`, `users_iduser`, `users_user_profile_idprofile`, `details_idDetails`),
+  INDEX `fk_property_info_users1_idx` (`users_iduser` ASC, `users_user_profile_idprofile` ASC) VISIBLE,
+  INDEX `fk_property_info_details1_idx` (`details_idDetails` ASC) VISIBLE,
+  CONSTRAINT `fk_property_info_users1`
+    FOREIGN KEY (`users_iduser` , `users_user_profile_idprofile`)
+    REFERENCES `db`.`users` (`iduser` , `user_profile_idprofile`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_property info_Details1`
-    FOREIGN KEY (`Details_idDetails`)
+  CONSTRAINT `fk_property_info_details1`
+    FOREIGN KEY (`details_idDetails`)
     REFERENCES `db`.`details` (`idDetails`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_property_info_coords1`
-    FOREIGN KEY (`coords_idcoords`)
-    REFERENCES `db`.`coords` (`idcoords`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -114,16 +96,26 @@ CREATE TABLE IF NOT EXISTS `db`.`image` (
   `url` VARCHAR(255) NOT NULL,
   `alt` VARCHAR(255) NOT NULL,
   `property_info_idProperty` INT NOT NULL,
-  `property_info_User_idUser` INT NOT NULL,
-  `property_info_User_table1_name` INT NOT NULL,
-  `property_info_Details_idDetails` INT NOT NULL,
-  PRIMARY KEY (`idimage`, `property_info_idProperty`, `property_info_User_idUser`, `property_info_User_table1_name`, `property_info_Details_idDetails`),
-  INDEX `fk_image_property_info1_idx` (`property_info_idProperty` ASC, `property_info_User_idUser` ASC, `property_info_User_table1_name` ASC, `property_info_Details_idDetails` ASC) VISIBLE,
+  `property_info_users_iduser` INT NOT NULL,
+  `property_info_users_user_profile_idprofile` INT NOT NULL,
+  PRIMARY KEY (`idimage`, `property_info_idProperty`, `property_info_users_iduser`, `property_info_users_user_profile_idprofile`),
+  INDEX `fk_image_property_info1_idx` (`property_info_idProperty` ASC, `property_info_users_iduser` ASC, `property_info_users_user_profile_idprofile` ASC) VISIBLE,
   CONSTRAINT `fk_image_property_info1`
-    FOREIGN KEY (`property_info_idProperty` , `property_info_User_idUser` , `property_info_User_table1_name` , `property_info_Details_idDetails`)
-    REFERENCES `db`.`property_info` (`idProperty` , `User_idUser` , `User_table1_name` , `Details_idDetails`)
+    FOREIGN KEY (`property_info_idProperty` , `property_info_users_iduser` , `property_info_users_user_profile_idprofile`)
+    REFERENCES `db`.`property_info` (`idProperty` , `users_iduser` , `users_user_profile_idprofile`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db`.`coords`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db`.`coords` (
+  `idcoords` INT NOT NULL,
+  `lat` DOUBLE NOT NULL,
+  `lang` DOUBLE NOT NULL,
+  PRIMARY KEY (`idcoords`))
 ENGINE = InnoDB;
 
 
